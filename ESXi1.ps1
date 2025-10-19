7211b7c3434c2e47364d27bbef812ab0bd9d343f
Nyukers Pro <nyukers@gmail.com>
2025-10-19 21:43:27 +0300

    Add files via upload

---
+++ ESXi1.ps1
@@ -0,0 +1,19 @@
+﻿ Install-Module -Name VMware.PowerCLI
+
+ Update-Module -Name VMware.PowerCLI
+
+ Get-ExecutionPolicy
+ Set-ExecutionPolicy -ExecutionPolicy Bypass
+ RemoteSigned
+
+ .\ESXi-Customizer-PS-v2.6.0.ps1 -sip –ozip
+ 
+ .\ESXi-Customizer-PS-v2.6.0.ps1 -v60 -vft -load net-tulip
+
+ F:\ESX\ESXi-Customizer-PS-v2.8.2.ps1 -v60 -vft -load net-tulip
+
+ F:\ESX\ESXi-Customizer-PS-v2.8.2.ps1 -sip –ozip
+ # N 255
+ F:\ESX\ESXi-Customizer-PS-v2.8.2.ps1 -iZip "F:\ESX\ESXi-6.0.0-20170604001-standard.zip" -vft -load net-tulip
+
+ Set-VMProcessor -VMName srv-esxi1 -ExposeVirtualizationExtensions $true
